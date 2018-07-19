a44948c9b5f75a4ff365fd40a77dc770e0480ede
Marco <foltin@tuta.io>
2018-07-19 19:12:39 +0200

    UI state above guard

--- StealthGame/Source/FPSGame/Private/FPSAIGuard.cpp
+++ StealthGame/Source/FPSGame/Private/FPSAIGuard.cpp
@@ -18,8 +18,11 @@ AFPSAIGuard::AFPSAIGuard()
 
 	PawnSensingComp->OnSeePawn.AddDynamic(this, &AFPSAIGuard::OnPawnSeen);
 	PawnSensingComp->OnHearNoise.AddDynamic(this, &AFPSAIGuard::OnNoiseHeard);
+
+	GuardState = EAIState::Idle;
 	
 }
+
 
 // Called when the game starts or when spawned
 void AFPSAIGuard::BeginPlay()
@@ -28,6 +31,7 @@ void AFPSAIGuard::BeginPlay()
 	
 	OriginalRotation = GetActorRotation();
 }
+
 
 void AFPSAIGuard::OnPawnSeen(APawn* SeenPawn)
 {
@@ -42,11 +46,19 @@ void AFPSAIGuard::OnPawnSeen(APawn* SeenPawn)
 		GM->CompleteMission(SeenPawn, false);
 	}
 
+	SetGuardState(EAIState::Alerted);
+
 	DrawDebugSphere(GetWorld(), SeenPawn->GetActorLocation(), 32.0f, 12, FColor::Red, false, 10.0f);
 }
 
+
 void AFPSAIGuard::OnNoiseHeard(APawn* NoiseInstigator, const FVector& Location, float Volume)
 {
+	if (GuardState = EAIState::Alerted)
+	{
+		return;
+	}
+
 	DrawDebugSphere(GetWorld(), Location, 32.0f, 12, FColor::Green, false, 10.0f);
 
 	FVector Direction = Location - GetActorLocation();
@@ -60,11 +72,36 @@ void AFPSAIGuard::OnNoiseHeard(APawn* NoiseInstigator, const FVector& Location, 
 
 	GetWorldTimerManager().ClearTimer(TimerHandle_ResetOrientation);
 	GetWorldTimerManager().SetTimer(TimerHandle_ResetOrientation, this, &AFPSAIGuard::ResetOrientation, 3.0f);
+
+	
+	SetGuardState(EAIState::Suspicious);
+	
 }
+
 
 void AFPSAIGuard::ResetOrientation()
 {
+	if (GuardState != EAIState::Alerted)
+	{
+		return;
+	}
+
 	SetActorRotation(OriginalRotation);
+
+	SetGuardState(EAIState::Idle);
+}
+
+
+void AFPSAIGuard::SetGuardState(EAIState NewState)
+{
+	if (GuardState == NewState)
+	{
+		return;
+	}
+
+	GuardState = NewState;
+
+	OnStateChanged(GuardState);
 }
 
 // Called every frame
