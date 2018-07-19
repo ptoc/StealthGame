014243ad76cc6fbc45b3422b3984df5c9b66b628
Marco <foltin@tuta.io>
2018-07-19 17:07:08 +0200

    Setup hearing sense

--- StealthGame/Source/FPSGame/Private/FPSAIGuard.cpp
+++ StealthGame/Source/FPSGame/Private/FPSAIGuard.cpp
@@ -5,6 +5,7 @@
 #include "DrawDebugHelpers.h"
 
 
+
 // Sets default values
 AFPSAIGuard::AFPSAIGuard()
 {
@@ -14,6 +15,7 @@ AFPSAIGuard::AFPSAIGuard()
 	PawnSensingComp = CreateDefaultSubobject<UPawnSensingComponent>(TEXT("PawnSensingComp"));
 
 	PawnSensingComp->OnSeePawn.AddDynamic(this, &AFPSAIGuard::OnPawnSeen);
+	PawnSensingComp->OnHearNoise.AddDynamic(this, &AFPSAIGuard::OnNoiseHeard);
 	
 }
 
@@ -31,7 +33,12 @@ void AFPSAIGuard::OnPawnSeen(APawn* SeenPawn)
 		return;
 	}
 
-	DrawDebugSphere(GetWorld(), SeenPawn->GetActorLocation(), 32.0f, 12, FColor::Yellow, false, 10.0f);
+	DrawDebugSphere(GetWorld(), SeenPawn->GetActorLocation(), 32.0f, 12, FColor::Red, false, 10.0f);
+}
+
+void AFPSAIGuard::OnNoiseHeard(APawn* NoiseInstigator, const FVector& Location, float Volume)
+{
+	DrawDebugSphere(GetWorld(), Location, 32.0f, 12, FColor::Green, false, 10.0f);
 }
 
 // Called every frame
--- StealthGame/Source/FPSGame/Public/FPSAIGuard.h
+++ StealthGame/Source/FPSGame/Public/FPSAIGuard.h
@@ -27,6 +27,10 @@ protected:
 	UFUNCTION()
 	void OnPawnSeen(APawn* SeenPawn);
 
+	UFUNCTION()
+	void OnNoiseHeard(APawn* NoiseInstigator, const FVector& Location, float Volume);
+
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
